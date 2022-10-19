d0c654423c66b5adcf86e480ba17aeb52ecca6ba
0xFirekeeper <0xFirekeeper@gmail.com>
2022-10-19 04:41:09 +0300

    Oasis Shop

--- contracts/OasisGraveyard.sol
+++ contracts/OasisGraveyard.sol
@@ -2,15 +2,22 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/interfaces/IERC721.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 contract OasisGraveyard {
     address public immutable crazyCamels;
+    address public immutable ost;
 
-    constructor(address _crazyCamels) {
+    constructor(address _crazyCamels, address _ost) {
         crazyCamels = _crazyCamels;
+        ost = _ost;
     }
 
-    function buriedCamels() public view returns (uint256) {
+    function buriedCamels() public view returns (uint256 crazyCamels_) {
         return IERC721(crazyCamels).balanceOf(address(this));
+    }
+
+    function buriedOasisTokens() public view returns (uint256 ost_) {
+        return IERC20(ost).balanceOf(address(this));
     }
 }
---
+++ contracts/OasisShop.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/interfaces/IERC721.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+contract OasisShop is Pausable, Ownable {
+    address public treasury;
+    address public ec;
+    address public ost;
+    uint256[] public ecTokenIds;
+    uint256 public ostPricePerEc;
+    address public oasisGraveyard;
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function setStateVariables(
+        address _treasury,
+        address _ec,
+        address _ost,
+        uint256[] memory _ecTokenIds,
+        uint256 _ostPricePerEc,
+        address _oasisGraveyard
+    ) public onlyOwner {
+        treasury = _treasury;
+        ec = _ec;
+        ost = _ost;
+        ecTokenIds = _ecTokenIds;
+        ostPricePerEc = _ostPricePerEc;
+        oasisGraveyard = _oasisGraveyard;
+    }
+
+    function purchaseEC() external whenNotPaused {
+        if (ecTokenIds.length < 1) revert("No available EC Token Ids");
+        if (IERC721(treasury).balanceOf(treasury) < 1) revert("Treasury has no ECs");
+        if (IERC20(ost).balanceOf(msg.sender) < ostPricePerEc) revert("Not Enough $OST Tokens");
+
+        uint256 lastTokenId = ecTokenIds[ecTokenIds.length - 1];
+
+        if (IERC20(ost).allowance(msg.sender, address(this)) < ostPricePerEc) revert("Not enough OST allowance");
+        IERC20(ost).transferFrom(msg.sender, oasisGraveyard, ostPricePerEc);
+
+        IERC721(ec).transferFrom(treasury, msg.sender, lastTokenId);
+        ecTokenIds.pop();
+    }
+
+    function getEcLeft() external view returns (uint256 ecsLeft_) {
+        return ecTokenIds.length;
+    }
+
+    function getOstPricePerEc() external view returns (uint256 ostPricePerEc_) {
+        return ostPricePerEc;
+    }
+}
